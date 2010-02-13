7a2573d4284d312b8c09bed6fc5bb2aa38bee710
David Rice <me@davidjrice.co.uk>
2010-02-13 02:15:06 +0000

    Tidy up

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require 'twitter'
 
+callback_url = 'http://208.75.85.139:4567/authorize'
+consumer_key = 'cV3cvjwaUW6GKoz55nkc8A'
+consumer_secret = '0q3N1wUJKjXeM5R84YhaymsEAFpPVbUoBEOwS3ThuAo'
+
 enable :sessions
 
 get '/' do
@@ -9,16 +13,16 @@ get '/' do
 end
 
 get '/connect' do
-  oauth = Twitter::OAuth.new('cV3cvjwaUW6GKoz55nkc8A', '0q3N1wUJKjXeM5R84YhaymsEAFpPVbUoBEOwS3ThuAo')
+  oauth = Twitter::OAuth.new(consumer_key, consumer_secret)
   session['token'] = oauth.request_token.token
   session['secret'] = oauth.request_token.secret
 
-  oauth.set_callback_url('http://208.75.85.139:4567/authorize')
+  oauth.set_callback_url(callback_url)
   redirect oauth.request_token.authorize_url
 end
 
 get '/authorize' do
-  oauth = Twitter::OAuth.new('cV3cvjwaUW6GKoz55nkc8A', '0q3N1wUJKjXeM5R84YhaymsEAFpPVbUoBEOwS3ThuAo')
+  oauth = Twitter::OAuth.new(consumer_key, consumer_secret)
   oauth.authorize_from_request(session['token'], session['secret'], params['oauth_verifier'])
 
   client = Twitter::Base.new(oauth)
